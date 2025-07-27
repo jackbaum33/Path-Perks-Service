85b083f05d5d568ca7b51cc612ad5fd295915c15
Jack Baum <jackbaum@umich.edu>
2025-07-27 12:11:06 -0400

    added template folder to flask app

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ from email.mime.multipart import MIMEMultipart
 load_dotenv()
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
-app = Flask(__name__)
+app = Flask(__name__ , template_folder='frontend')
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/')
